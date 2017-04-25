cf0306fc9060eeddae44e9d15cc7b5eaa80e9bf1
Varanetskaya <creativeanastasia@gmail.com>
2017-04-25 16:48:31 -0700

    deleting uploads

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,28 +1,40 @@
 class UploadsController < ApplicationController
-    def index
-      @uploads = Upload.all
-    end
+  def index
+    @uploads = Upload.all
+  end
 
-    def show
-       @upload = Upload.find(params[:id])
-    end
+  def show
+     @upload = Upload.find(params[:id])
+  end
 
-    def new
-      @upload = Upload.new
-    end
+  def new
+    @upload = Upload.new
+  end
 
-    def create
-      @upload = Upload.create(upload_params)
-      @upload.user = current_user
-      if @upload.save
-        redirect_to uploads_path
-      else
-        render :new
-      end
-    end
-
-  private
-    def upload_params
-      params.require(:upload).permit(:description, :image)
+  def create
+    @upload = Upload.create(upload_params)
+    @upload.user = current_user
+    if @upload.save
+      redirect_to uploads_path
+    else
+      render :new
     end
   end
+
+  def destroy
+    @upload = Upload.find(params[:id])
+      if @upload.destroy
+        flash[:notice] = "Successfully deleted photo!"
+        redirect_to uploads_path
+      else
+        flash[:alert] = "Error deleting photo!"
+      end
+  end
+
+private
+  def upload_params
+    params.require(:upload).permit(:description, :image)
+  end
+end
+
+
